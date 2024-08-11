e55341ba8d79c78488008ec0f88bdb469849883e
cpmoore0317 <cpmoore317@gmail.com>
2024-08-11 11:55:31 -0500

    Fixing spacing for scripts

--- js/script.js
+++ js/script.js
@@ -9,42 +9,42 @@
 
 window.addEventListener('DOMContentLoaded', event => {
 
-  // Activate Bootstrap scrollspy on the main nav element
-  const mainNav = document.body.querySelector('#mainNav');
-  if (mainNav) {
-      new bootstrap.ScrollSpy(document.body, {
-          target: '#mainNav',
-          rootMargin: '0px 0px -40%',
-      });
-  };
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
 
-  // Collapse responsive navbar when toggler is visible
-  const navbarToggler = document.body.querySelector('.navbar-toggler');
-  const responsiveNavItems = [].slice.call(
-      document.querySelectorAll('#navbarResponsive .nav-link')
-  );
-  responsiveNavItems.map(function (responsiveNavItem) {
-      responsiveNavItem.addEventListener('click', () => {
-          if (window.getComputedStyle(navbarToggler).display !== 'none') {
-              navbarToggler.click();
-          }
-      });
-  });
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
 
 });
 
 document.querySelectorAll('.menu-item').forEach(item => {
-  item.addEventListener('click', () => {
-    const circleMenu = document.querySelector('.circle-menu');
-    circleMenu.classList.add('spin');
-
-    // Remove the 'spin' class after 2 seconds to stop the animation
-    setTimeout(() => {
-      circleMenu.classList.remove('spin');
-    }, 2000);
+    item.addEventListener('click', () => {
+      const circleMenu = document.querySelector('.circle-menu');
+      circleMenu.classList.add('spin');
+  
+      // Remove the 'spin' class after 2 seconds to stop the animation
+      setTimeout(() => {
+        circleMenu.classList.remove('spin');
+      }, 2000);
+    });
   });
-});
-
+  
 let slideIndex = 1;
 showSlides(slideIndex);
 
